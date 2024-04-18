f699ab361e4bbe17d081d94c443956dd935c83db
anand <anandpromax@gmail.com>
2024-04-18 12:52:39 +0530

    Fix image path in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ function Hero({ skills, aboutUser, socialHandles }) {
      
     return (
         <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
-            <img src="../../public/hero.png" alt="Hero" width={1572} height={795} className="absolute -top-[98px] -z-10" />
+            <img src="public/hero.png" alt="Hero" width={1572} height={795} className="absolute -top-[98px] -z-10" />
             <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
                 <motion.article variants={slideInFromTop} initial="initial" animate="animate">
                     <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
